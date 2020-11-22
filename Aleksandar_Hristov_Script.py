8d4b0ba954e92f5c4a7f83301efa899cec610526
Aleksandar Hristov <sashkinaaa@gmail.com>
2020-11-22 08:31:34 +0000

    Upgrade script

--- Aleksandar_Hristov_Script.py
+++ Aleksandar_Hristov_Script.py
@@ -1,17 +1,29 @@
 class Calculator():
 
-    def aPlusb(a,b):
-        c = a + b
+    def __init__ (self, a, b):
+        self.a = a
+        self.b = b
+
+    def aPlusb(self):
+        c = self.a + self.b
         return c
 
-    def aMinusb(a,b):
-        c = a - b
+    def aMinusb(self):
+        c = self.a - self.b
         return c
 
-    def aMumltiplicateb(a,b):
-        c = a * b
+    def aMumltiplicateb(self):
+        c = self.a * self.b
         return c
 
 if __name__ == "__main__":
-    c = Calculator.aPlusb(int(input("a=")),int(input("b=")))
-    print(c)
+    c = Calculator(int(input("a=")),int(input("b=")))
+    operator = input("operator: ")
+    if operator == '+':
+        print(c.aPlusb())
+    elif operator == '-':
+        print(c.aMinusb())
+    elif operator == '*':
+        print(c.aMumltiplicateb())
+    else:
+        print("Wrong operation")
