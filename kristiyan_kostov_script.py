66a4af088ccbaf02e860e51658283cb929fe1385
kik4444 <krisi.s.kostov@gmail.com>
2020-11-22 10:38:08 +0200

    New approach with operator

--- kristiyan_kostov_script.py
+++ kristiyan_kostov_script.py
@@ -1,3 +1,11 @@
+import operator
+ops = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv
+}
+
 class Calculator():
 
     def __init__(self, a, b, action):
@@ -5,30 +13,15 @@ class Calculator():
         self.b = b
         self.action = action
     
-    def add(self):
-        return self.a + self.b
-
-    def sub(self):
-        return self.a - self.b
-
-    def mul(self):
-        return self.a * self.b
-
-    def div(self):
-        return self.a / self.b
+    def calculate(self):
+        return ops[self.action](self.a, self.b)
 
 if __name__ == "__main__":
     calc = Calculator(int(input("Input a: ")), int(input("Input b: ")), input("Input action as \"+\" \"-\" \"*\" or \"/\": "))
     
-    if (calc.action == "+"):
-        result = calc.add()
-    elif (calc.action == "-"):
-        result = calc.sub()
-    elif (calc.action == "*"):
-        result = calc.mul()
-    elif (calc.action == "/"):
-        result = calc.div()
-    else:
+    if (calc.action != "+" and calc.action != "-" and calc.action != "*" and calc.action != "/"):
         result = "***ERROR***"
+    else:
+        result = calc.calculate()
     
     print("Result is: " + str(result))
