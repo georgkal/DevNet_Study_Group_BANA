38af9acf2e541a536385ef9b8ec38d1effeb0f56
ruslan.aleksiev <ruslan.alexiev@gmail.com>
2020-11-22 10:20:48 +0200

    adding class&method lab

---
+++ ruslan_aleksiev_script.py
@@ -0,0 +1,27 @@
+class Calculate():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    
+    def addition(self):
+        return self.a + self.b
+    def divide(self):
+        return self.a - self.b
+    def multiply(self):
+        return self.a * self.b
+
+input_a = int(input("Enter first digit: "))
+input_b = int(input("Enter seconf digit: "))
+input_oper = input("Enter operator + , - or *: ")
+
+if  input_oper == "+":
+    add = Calculate(input_a, input_b)
+    print(add.addition())
+elif input_oper == "-":
+    div = Calculate(input_a, input_b)
+    print(div.divide())
+elif input_oper == "*":
+    mlt = Calculate(input_a, input_b)
+    print(mlt.multiply())
+else:
+    print("Wrong operator, try again!")
